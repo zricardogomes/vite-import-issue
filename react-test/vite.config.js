0d435529082d5f89f479722e4f90cc1427e26441
Ricardo Gomes <ricardo.gomes@regincontrols.pt>
2023-10-11 18:06:06 +0100

    vite-lib is working on react-test even though it is a bit ugly the process, since the worker-entry needs to be copied into the root.

--- react-test/vite.config.js
+++ react-test/vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()],
-  //optimizeDeps: {
-  //  exclude: ['exosocket'],
-  //},
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./node_modules/exosocket/dist/worker-entry.js",
+          dest: "./",
+        },
+      ],
+    }),
+  ],
 });
